d26c0084dec950a6884a9e2a2ca218ab7d11bcc7
joelparavara <joelparavara@gmail.com>
2020-05-29 22:53:06 +0530

    Initial Commit
    Updated Scene Delegate for programatic UI
    
    Removed Stroyboard

---
+++ intro-pages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intro-pages
+//
+//  Created by Joel Thomson on 5/29/20.
+//  Copyright © 2020 Techcrus Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
