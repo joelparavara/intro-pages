764a1de29de96922fc3eb8d6040d198d25625f1d
joelparavara <joelparavara@gmail.com>
2020-06-11 23:45:55 +0530

    updated with new Auto Layout

--- intro-pages/ViewController.swift
+++ intro-pages/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+      
+    let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "welcome.jpg"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let descriptionTextView : UITextView = {
+        let textView = UITextView()
+        textView.text = "Join us today in our fun and games"
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = UIColor.init(displayP3Red: 248, green: 248, blue: 241, alpha: 5)
+    
+        setupLayout()
+        
     }
-
-
+    
+    private func setupLayout() {
+        let topImageContainerView = UIView()
+        topImageContainerView.backgroundColor = .blue
+        view.addSubview(topImageContainerView)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Setup Autolayout for topImageContainerView
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        topImageContainerView.addSubview(imageView)
+        // Setup Constrains for Image View
+        imageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 350).isActive = true
+        imageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.8).isActive = true
+        
+        topImageContainerView.addSubview(descriptionTextView)
+        // Setup Constrains for Text View
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 10).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 120).isActive = true
+    }
+    
 }
-
